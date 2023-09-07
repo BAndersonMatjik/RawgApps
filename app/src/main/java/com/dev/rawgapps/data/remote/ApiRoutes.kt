74957e603953cf46e67d62cc016c18bb780faab2
Billy Anderson Matjik <bmatjik@gmail.com>
2023-09-08 04:23:19 +0700

    + add Endpoint Games

--- app/src/main/java/com/dev/rawgapps/data/remote/ApiRoutes.kt
+++ app/src/main/java/com/dev/rawgapps/data/remote/ApiRoutes.kt
@@ -3,4 +3,6 @@ package com.dev.rawgapps.data.remote
 object ApiRoutes {
     const val BASE_URL = "api.rawg.io/api"
     const val API_KEY = "e3078a9167444ca49a529d0e6f3bdff2"
+
+    const val ENDPOINT_GAMES = "games"
 }
