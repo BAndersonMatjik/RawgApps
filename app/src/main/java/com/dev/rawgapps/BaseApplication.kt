a978ce9ffbd41c6dd77988514cd79b15fec5f519
Billy Anderson Matjik <bmatjik@gmail.com>
2023-09-07 20:12:42 +0700

    + Add Handling timber

--- app/src/main/java/com/dev/rawgapps/BaseApplication.kt
+++ app/src/main/java/com/dev/rawgapps/BaseApplication.kt
@@ -1,8 +1,17 @@
 package com.dev.rawgapps
 
 import android.app.Application
+import com.dev.rawgapps.common.isApplicationDebuggable
 import dagger.hilt.android.HiltAndroidApp
+import timber.log.Timber
 
 @HiltAndroidApp
 class BaseApplication: Application() {
+    override fun onCreate() {
+        super.onCreate()
+        //Setup Timber Just Running on Debug Mode
+        if (isApplicationDebuggable()){
+            Timber.plant(Timber.DebugTree())
+        }
+    }
 }
