94ed685b80592e129d05f040d004bf97b6b37177
Billy Anderson Matjik <bmatjik@gmail.com>
2023-09-09 05:36:39 +0700

    + Add Many Preview and One Preview

--- app/src/main/java/com/dev/rawgapps/common/DevicesPreviews.kt
+++ app/src/main/java/com/dev/rawgapps/common/DevicesPreviews.kt
@@ -5,3 +5,6 @@ import androidx.compose.ui.tooling.preview.Preview
 @Preview(name = "phone", device = "spec:shape=Normal,width=360,height=640,unit=dp,dpi=480", showBackground = true)
 @Preview(name = "tablet", device = "spec:shape=Normal,width=1280,height=800,unit=dp,dpi=480", showBackground = true)
 annotation class DevicePreviews()
+
+@Preview(name = "phone", device = "spec:shape=Normal,width=360,height=640,unit=dp,dpi=480", showBackground = true)
+annotation class phonePreviews()
