1564367d42017dc98c9e8a6e25f1fb3f7c6b7ee6
Billy Anderson Matjik <bmatjik@gmail.com>
2023-09-08 17:39:19 +0700

    +add UrlUtils for Build endpoint and query parameter

---
+++ app/src/main/java/com/dev/rawgapps/common/UrlUtils.kt
@@ -0,0 +1,16 @@
+package com.dev.rawgapps.common
+
+import io.ktor.http.ParametersBuilder
+import io.ktor.http.URLBuilder
+import io.ktor.http.Url
+
+object UrlUtils {
+    fun createRequestUrl(endPoint:String,parameterBuilder:(ParametersBuilder)->Unit): Url {
+        val urlBuilder = URLBuilder()
+        urlBuilder.path(endPoint)
+        urlBuilder.parameters.apply {
+            parameterBuilder(this)
+        }
+        return urlBuilder.build()
+    }
+}
