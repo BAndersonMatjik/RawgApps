58ad9b005dd49296d1277fdc6cf1e484245e73ac
Billy Anderson Matjik <bmatjik@gmail.com>
2023-09-09 04:28:50 +0700

    !Change MainActivity call to MainScreen

--- app/src/main/java/com/dev/rawgapps/MainActivity.kt
+++ app/src/main/java/com/dev/rawgapps/MainActivity.kt
@@ -1,46 +1,51 @@
 package com.dev.rawgapps
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.lifecycleScope
+import androidx.navigation.compose.rememberNavController
+import com.dev.rawgapps.common.isApplicationDebuggable
+import com.dev.rawgapps.data.remote.ApiRoutes
+import com.dev.rawgapps.data.remote.NetworkHandler
+import com.dev.rawgapps.data.remote.NetworkStatus
+import com.dev.rawgapps.data.remote.datasource.RawgRemoteDataSourceImpl
 import com.dev.rawgapps.ui.theme.RawgAppsTheme
+import dagger.hilt.android.AndroidEntryPoint
+import io.ktor.client.HttpClient
+import io.ktor.client.request.get
+import io.ktor.http.URLBuilder
+import io.ktor.http.Url
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             RawgAppsTheme {
+                val navHostController = rememberNavController()
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    MainScreen(navController = navHostController)
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    RawgAppsTheme {
-        Greeting("Android")
     }
 }
