4aff943303ea204d01653630fab0f9761c5b29cf
Billy Anderson Matjik <bmatjik@gmail.com>
2023-09-09 07:17:42 +0700

    !Refactor PhonePreviews

---
+++ app/src/main/java/com/dev/rawgapps/common/PhonePreviews.kt
@@ -0,0 +1,6 @@
+package com.dev.rawgapps.common
+
+import androidx.compose.ui.tooling.preview.Preview
+
+@Preview(name = "phone", device = "spec:shape=Normal,width=360,height=640,unit=dp,dpi=480", showBackground = true)
+annotation class PhonePreviews()
