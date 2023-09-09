34fe3d857343bb0e97c51f93bf333a56c390072c
Billy Anderson Matjik <bmatjik@gmail.com>
2023-09-10 04:34:39 +0700

    ! Refactor Preview

--- app/src/main/java/com/dev/rawgapps/ui/GameItemUi.kt
+++ app/src/main/java/com/dev/rawgapps/ui/GameItemUi.kt
@@ -112,7 +112,8 @@ fun GameCardPreview() {
         genre = listOf("Action", "Rpg"),
         released = "2013-09-17",
         backgroundImage = "https://media.rawg.io/media/games/20a/20aa03a10cda45239fe22d035c0ebe64.jpg",
-        description = "?"
+        description = "?",
+        developer = "developer"
     )
     GameCard(
         game = fake, modifier = Modifier
