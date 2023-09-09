93edaac72f5d91eaf221043d497105ea2a8802a8
Billy Anderson Matjik <bmatjik@gmail.com>
2023-09-09 10:00:15 +0700

    + Add handling debounce

--- app/src/main/java/com/dev/rawgapps/ui/GameItemUi.kt
+++ app/src/main/java/com/dev/rawgapps/ui/GameItemUi.kt
@@ -5,30 +5,24 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowBack
-import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardColors
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
-import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalInspectionMode
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
@@ -38,7 +32,6 @@ import coil.request.ImageRequest
 import com.dev.rawgapps.R
 import com.dev.rawgapps.common.CustomFontFamily
 import com.dev.rawgapps.domain.Game
-import com.dev.rawgapps.ui.theme.RawgAppsTheme
 
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -49,6 +42,14 @@ fun GameCard(
     cardColors: CardColors = CardDefaults.cardColors(containerColor = Color.White),
     onItemClick:(Game)->Unit={}
 ) {
+    var isClick by remember{
+        mutableStateOf(false)
+    }
+    isClick.useDebounce(delayMillis = 200L,onChange = {
+        if (it){
+            onItemClick(game)
+        }
+    })
     Card(
         modifier = modifier.shadow(
             ambientColor = Color.Gray.copy(alpha = 0.8F),
@@ -56,7 +57,7 @@ fun GameCard(
         ), colors = cardColors,
         shape = CardDefaults.elevatedShape,
         onClick ={
-            onItemClick(game)
+            isClick = true
         }
     ) {
         AsyncImage(
