d2de10b5912075b302a0c7ac2eb49f4895da2a81
Billy Anderson Matjik <bmatjik@gmail.com>
2023-09-09 19:44:32 +0700

    +Add From ResourceJson2Bean

--- app/src/test/java/com/dev/rawgapps/utils/ResourceUtils.kt
+++ app/src/test/java/com/dev/rawgapps/utils/ResourceUtils.kt
@@ -1,5 +1,8 @@
 package com.dev.rawgapps.utils
 
+import com.dev.rawgapps.data.remote.model.GamesResponse
+import kotlinx.serialization.decodeFromString
+import kotlinx.serialization.json.Json
 import java.io.BufferedReader
 import java.io.InputStreamReader
 import java.util.stream.Collectors
@@ -12,6 +15,23 @@ object ResourceUtils {
             val result = bufferReader.lines().parallel().collect(Collectors.joining())
             inputStream.close()
             result
+        }
+    }
+
+    private val json = Json {
+        prettyPrint = true
+        isLenient = true
+        ignoreUnknownKeys = true
+    }
+
+    fun getResource2Bean(fileName:String): GamesResponse? {
+        return javaClass.classLoader?.getResourceAsStream(fileName)?.let {
+            val inputStream = InputStreamReader(it, Charsets.UTF_8)
+            val bufferReader = BufferedReader(inputStream)
+            val result = bufferReader.lines().parallel().collect(Collectors.joining())
+            val resultBean = json.decodeFromString<GamesResponse>(result)
+            inputStream.close()
+            resultBean
         }
     }
 }
