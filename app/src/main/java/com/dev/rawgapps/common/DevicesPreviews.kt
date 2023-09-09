c765e32b11f9f10913fcff96dc21c96bfb2f7bbd
Billy Anderson Matjik <bmatjik@gmail.com>
2023-09-09 07:06:25 +0700

    !Refactor annotation Class phonePreviews to PhonePreviews

--- app/src/main/java/com/dev/rawgapps/common/DevicesPreviews.kt
+++ app/src/main/java/com/dev/rawgapps/common/DevicesPreviews.kt
@@ -7,4 +7,4 @@ import androidx.compose.ui.tooling.preview.Preview
 annotation class DevicePreviews()
 
 @Preview(name = "phone", device = "spec:shape=Normal,width=360,height=640,unit=dp,dpi=480", showBackground = true)
-annotation class phonePreviews()
+annotation class PhonePreviews()
