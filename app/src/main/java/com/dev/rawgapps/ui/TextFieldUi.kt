317acba8916db7fa166c93a8596118b011824a27
Billy Anderson Matjik <bmatjik@gmail.com>
2023-09-09 09:43:08 +0700

    +Add Debounce onTextChange Result

--- app/src/main/java/com/dev/rawgapps/ui/TextFieldUi.kt
+++ app/src/main/java/com/dev/rawgapps/ui/TextFieldUi.kt
@@ -1,28 +1,24 @@
 package com.dev.rawgapps.ui
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardColors
 import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.CardElevation
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.rememberUpdatedState
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
@@ -36,17 +32,25 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.dev.rawgapps.common.CustomFontFamily
 import com.dev.rawgapps.ui.theme.RawgAppsTheme
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 @Composable
 fun SearchTextField(
     modifier: Modifier = Modifier,
     cardColors: CardColors = CardDefaults.cardColors(containerColor = Color.White),
-    hint:String = "Hint"
+    hint:String = "Hint",
+    onTextChange:(String)->Unit={}
 ) {
     var text by rememberSaveable { mutableStateOf("") }
     var isFocus by rememberSaveable {
         mutableStateOf(false)
     }
+
+    text.useDebounce(onChange = {
+        onTextChange(it)
+    })
     Card(modifier = modifier
         .shadow(ambientColor = Color.Gray.copy(alpha = 0.8F), elevation = 10.dp)
         .onFocusChanged {
@@ -77,6 +81,28 @@ fun SearchTextField(
     }
 }
 
+@Composable
+fun <T> T.useDebounce(
+    delayMillis: Long = 400L,
+    // 1. couroutine scope
+    coroutineScope: CoroutineScope = rememberCoroutineScope(),
+    onChange: (T) -> Unit
+): T{
+    // 2. updating state
+    val state by rememberUpdatedState(this)
+
+    // 3. launching the side-effect handler
+    DisposableEffect(state){
+        val job = coroutineScope.launch {
+            delay(delayMillis)
+            onChange(state)
+        }
+        onDispose {
+            job.cancel()
+        }
+    }
+    return state
+}
 @Preview(showBackground = true, device = Devices.NEXUS_5)
 @Composable
 fun SearchTextFieldPreview() {
