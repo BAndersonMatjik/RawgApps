6132832ece4e4a93d21238fcb97b9111ee6e9c9a
Billy Anderson Matjik <bmatjik@gmail.com>
2023-09-11 05:19:35 +0700

    !Combine 2 Stream data

--- app/src/main/java/com/dev/rawgapps/domain/usecase/GetDetailUsecaseImpl.kt
+++ app/src/main/java/com/dev/rawgapps/domain/usecase/GetDetailUsecaseImpl.kt
@@ -4,15 +4,22 @@ import com.dev.rawgapps.data.repository.RawgRepository
 import com.dev.rawgapps.domain.Game
 import dagger.hilt.android.scopes.ViewModelScoped
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.flow.flow
 import javax.inject.Inject
 
 @ViewModelScoped
 class GetDetailUsecaseImpl @Inject constructor(private val rawgRepository: RawgRepository) :
     GetDetailGameUsecase {
-    override suspend fun invoke(slug: String): Flow<Result<Game>> {
+    override suspend operator fun invoke(slug: String): Flow<Result<Game>> {
         return flow {
-            emit(rawgRepository.getGame(slug))
+            combine(rawgRepository.getGame(slug),rawgRepository.getGameFavorite(slug)){game,gameFavorite->
+                if (gameFavorite==null){
+                    emit(game)
+                }else{
+                    emit(gameFavorite)
+                }
+            }
         }
     }
 }
