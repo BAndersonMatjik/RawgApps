2d1ebc307d936fd1f220ea5fdef1756d8bc5c6be
Billy Anderson Matjik <bmatjik@gmail.com>
2023-09-11 04:44:59 +0700

    +Add Local Data source logic getGameBySlag

--- app/src/main/java/com/dev/rawgapps/data/local/datasource/RawgLocalDataSourceImpl.kt
+++ app/src/main/java/com/dev/rawgapps/data/local/datasource/RawgLocalDataSourceImpl.kt
@@ -1,10 +1,22 @@
 package com.dev.rawgapps.data.local.datasource
 
+import com.dev.rawgapps.common.JsonUtils
+import com.dev.rawgapps.data.local.dao.FavoriteDao
 import com.dev.rawgapps.domain.Game
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
+import kotlinx.serialization.ExperimentalSerializationApi
+import kotlinx.serialization.decodeFromString
+import javax.inject.Inject
+import javax.inject.Singleton
 
-class RawgLocalDataSourceImpl:RawgLocalDataSource {
-    override fun getGameBySlag(slag: String): Flow<Game> {
-        TODO("Not yet implemented")
+@Singleton
+class RawgLocalDataSourceImpl @Inject constructor(
+    private val dao: FavoriteDao
+):RawgLocalDataSource {
+
+    @OptIn(ExperimentalSerializationApi::class)
+    override fun getGameBySlag(slag: String): Flow<Game> =dao.findBySlag(slag).map {
+        JsonUtils.json.decodeFromString<Game>(string = it.jsonContent)
     }
 }
