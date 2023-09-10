f98687c61feb7e970023bca98cbab32a850ccee6
Billy Anderson Matjik <bmatjik@gmail.com>
2023-09-11 06:03:15 +0700

    !Change Logic support 2 flow data and give priority to local data

--- app/src/main/java/com/dev/rawgapps/domain/usecase/GetDetailUsecaseImpl.kt
+++ app/src/main/java/com/dev/rawgapps/domain/usecase/GetDetailUsecaseImpl.kt
@@ -5,20 +5,27 @@ import com.dev.rawgapps.domain.Game
 import dagger.hilt.android.scopes.ViewModelScoped
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.combine
-import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.distinctUntilChanged
+import timber.log.Timber
 import javax.inject.Inject
 
 @ViewModelScoped
 class GetDetailUsecaseImpl @Inject constructor(private val rawgRepository: RawgRepository) :
     GetDetailGameUsecase {
+    companion object {
+        private const val TAG = "GetDetailUsecaseImpl"
+    }
+
     override suspend operator fun invoke(slug: String): Flow<Result<Game>> {
-        return flow {
-            combine(rawgRepository.getGame(slug),rawgRepository.getGameFavorite(slug)){game,gameFavorite->
-                if (gameFavorite==null){
-                    emit(game)
-                }else{
-                    emit(gameFavorite)
-                }
+        return combine(
+            rawgRepository.getGameFavorite(slug).distinctUntilChanged(),
+            rawgRepository.getGame(slug)
+        ) { gameFavorite,game ->
+            Timber.d("invoke: $game --- $gameFavorite")
+            if (gameFavorite.isFailure) {
+                game
+            } else {
+                gameFavorite
             }
         }
     }
