efc4749daf124599f0c79fbdd3b52f11a5eb8de6
Billy Anderson Matjik <bmatjik@gmail.com>
2023-09-10 19:18:10 +0700

    !Change Params to varargs

--- app/src/main/java/com/dev/rawgapps/data/remote/datasource/PagingSourceFactory.kt
+++ app/src/main/java/com/dev/rawgapps/data/remote/datasource/PagingSourceFactory.kt
@@ -4,14 +4,14 @@ import dagger.hilt.android.scopes.ViewModelScoped
 import javax.inject.Inject
 
 interface PageDataSourceFacade<T>{
-    fun create(keyword: String):T
+    fun create(vararg params:String):T
 }
 
 
 @ViewModelScoped
 class GamePageDataSourceFacade @Inject constructor(private val rawgRemoteDataSource: RawgRemoteDataSource):PageDataSourceFacade<GamePagingDataSource>{
-    override fun create(keyword: String): GamePagingDataSource {
-        return GamePagingDataSource( rawgRemoteDataSource = rawgRemoteDataSource, query = keyword)
+    override fun create(vararg params:String): GamePagingDataSource {
+        return GamePagingDataSource( rawgRemoteDataSource = rawgRemoteDataSource, query = params[0])
     }
 
 }
