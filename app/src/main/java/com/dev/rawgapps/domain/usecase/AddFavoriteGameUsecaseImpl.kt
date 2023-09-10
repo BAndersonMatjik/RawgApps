14e59fd0e882a2c6ac98bcb82b67634da3255caa
Billy Anderson Matjik <bmatjik@gmail.com>
2023-09-11 06:38:23 +0700

    !move process to io dispacther

--- app/src/main/java/com/dev/rawgapps/domain/usecase/AddFavoriteGameUsecaseImpl.kt
+++ app/src/main/java/com/dev/rawgapps/domain/usecase/AddFavoriteGameUsecaseImpl.kt
@@ -3,16 +3,20 @@ package com.dev.rawgapps.domain.usecase
 import com.dev.rawgapps.data.repository.RawgRepository
 import com.dev.rawgapps.domain.Game
 import dagger.hilt.android.scopes.ViewModelScoped
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 @ViewModelScoped
 class AddFavoriteGameUsecaseImpl @Inject constructor(private val rawgRepository: RawgRepository) :
     AddFavoriteGameUsecase {
     override suspend fun invoke(game: Game) {
-        if (!game.isFavorite){
-            rawgRepository.addFavoriteGame(game)
-        }else{
-            rawgRepository.removeFavoriteGame(slug = game.slug)
+        withContext(Dispatchers.IO){
+            if (!game.isFavorite){
+                rawgRepository.addFavoriteGame(game)
+            }else{
+                rawgRepository.removeFavoriteGame(slug = game.slug)
+            }
         }
     }
 }
