8610588f705404ad91f9215e98ae584f15c7e57c
Billy Anderson Matjik <bmatjik@gmail.com>
2023-09-11 04:24:20 +0700

    + add Dao

---
+++ app/src/main/java/com/dev/rawgapps/data/local/dao/FavoriteDao.kt
@@ -0,0 +1,22 @@
+package com.dev.rawgapps.data.local.dao
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.dev.rawgapps.domain.Game
+import kotlinx.coroutines.flow.Flow
+
+@Dao()
+interface FavoriteDao {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insertAll(vararg game: Game)
+
+    @Delete
+    fun delete(game:Game)
+
+    @Query("SELECT * FROM favorite_game WHERE slag = :slag")
+    suspend fun findBySlag(slag:String): Flow<Game>
+}
