71f13d654d18f8b4e8d8872b63a37634e67a4ed8
Billy Anderson Matjik <bmatjik@gmail.com>
2023-09-12 01:36:40 +0700

    !Add Viewmodel and event for handling state ui favorite game

---
+++ app/src/main/java/com/dev/rawgapps/feature/favoritegame/FavoriteGameViewModel.kt
@@ -0,0 +1,45 @@
+package com.dev.rawgapps.feature.favoritegame
+
+import androidx.lifecycle.viewModelScope
+import androidx.paging.PagingData
+import androidx.paging.cachedIn
+import com.dev.rawgapps.common.BaseViewModel
+import com.dev.rawgapps.domain.Game
+import com.dev.rawgapps.domain.usecase.GetFavoriteGamesUsecase
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.flow.distinctUntilChanged
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class FavoriteGameViewModel @Inject constructor(
+    private val getFavoriteGamesUsecase: GetFavoriteGamesUsecase
+) : BaseViewModel<FavoriteGameViewModel.FavoriteGameEvent>() {
+    private val _gameState: MutableStateFlow<PagingData<Game>> =
+        MutableStateFlow(value = PagingData.empty())
+    val gamesState: MutableStateFlow<PagingData<Game>> get() = _gameState
+    init {
+        onEvent(FavoriteGameViewModel.FavoriteGameEvent.GetFavoriteGames)
+    }
+    sealed class FavoriteGameEvent {
+        object GetFavoriteGames : FavoriteGameEvent()
+    }
+
+    override fun onEvent(event: FavoriteGameEvent) {
+        viewModelScope.launch {
+            when (event) {
+                is FavoriteGameEvent.GetFavoriteGames -> {
+                    getFavoriteGamesUsecase().distinctUntilChanged().cachedIn(viewModelScope)
+                        .collectLatest {
+                            _gameState.value = it
+                        }
+                }
+                else -> {
+
+                }
+            }
+        }
+    }
+}
