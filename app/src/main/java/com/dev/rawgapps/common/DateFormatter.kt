3b3a9e10604bf33be0afe407563310b9ed38f7d7
Billy Anderson Matjik <bmatjik@gmail.com>
2023-09-12 02:44:28 +0700

    !Handling if date is string empty

--- app/src/main/java/com/dev/rawgapps/common/DateFormatter.kt
+++ app/src/main/java/com/dev/rawgapps/common/DateFormatter.kt
@@ -5,6 +5,9 @@ import java.text.SimpleDateFormat
 object DateFormatter {
     //If null will give back value from parameter
     fun formatDate(date:String,oldPattern:String="yyyy-MM-dd",newPattern:String="MMM dd,yyyy"):String{
+        if (date.isBlank()){
+            return date
+        }
         var formatter = SimpleDateFormat(oldPattern)
         val date = formatter.parse(date)
         formatter  = SimpleDateFormat(newPattern)
