c770056fdebd9002767f75ef3fb389c2393b171b
Billy Anderson Matjik <bmatjik@gmail.com>
2023-09-13 00:19:47 +0700

    !Refactor

--- app/src/test/java/com/dev/rawgapps/GamesScreenTest.kt
+++ app/src/test/java/com/dev/rawgapps/GamesScreenTest.kt
@@ -3,14 +3,17 @@ package com.dev.rawgapps
 import androidx.activity.compose.setContent
 import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import androidx.compose.ui.test.onNodeWithTag
 import androidx.compose.ui.test.onNodeWithText
 import androidx.compose.ui.test.onRoot
+import androidx.compose.ui.test.performTextInput
 import androidx.compose.ui.test.printToLog
 import androidx.navigation.compose.rememberNavController
 import com.dev.rawgapps.ui.theme.RawgAppsTheme
 import dagger.hilt.android.testing.HiltAndroidRule
 import dagger.hilt.android.testing.HiltAndroidTest
 import dagger.hilt.android.testing.HiltTestApplication
+import org.junit.After
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
@@ -22,7 +25,7 @@ import org.robolectric.shadows.ShadowLog
 @HiltAndroidTest
 @RunWith(RobolectricTestRunner::class)
 @Config(application = HiltTestApplication::class, manifest = Config.NONE)
-class MainActivityTest {
+class GamesScreenTest {
 
     @get:Rule(order = 0)
     val hiltRule = HiltAndroidRule(this)
@@ -36,13 +39,19 @@ class MainActivityTest {
         ShadowLog.stream = System.out // Redirect Logcat to console
     }
 
+    @After
+    fun cleanUp(){
+        ShadowLog.stream = null
+    }
+
     @Test
-    fun when_I_test_then_it_works2() {
+    fun validate_ui_text_then_it_works() {
         composeTestRule.onRoot().printToLog("TAG")
         composeTestRule.onNodeWithText("Rawg games".uppercase(), useUnmergedTree = true)
             .assertIsDisplayed()
         composeTestRule.onNodeWithText("Search Games", useUnmergedTree = true)
             .assertIsDisplayed()
+        composeTestRule.onNodeWithTag("text_field_search").performTextInput("GTA")
     }
 
     @Test
@@ -54,9 +63,7 @@ class MainActivityTest {
                 MainScreen(navController = rememberNavController())
             }
         }
-
         composeTestRule.onNodeWithText("Grand Theft Auto V").assertIsDisplayed()
-
     }
 
 
