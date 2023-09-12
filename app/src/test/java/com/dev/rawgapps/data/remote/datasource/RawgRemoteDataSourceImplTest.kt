e57b54b04efbfcfd14b3917cf01ca175d2f4b5f2
Billy Anderson Matjik <bmatjik@gmail.com>
2023-09-12 21:48:46 +0700

    + add new unit test  get detail game

--- app/src/test/java/com/dev/rawgapps/data/remote/datasource/RawgRemoteDataSourceImplTest.kt
+++ app/src/test/java/com/dev/rawgapps/data/remote/datasource/RawgRemoteDataSourceImplTest.kt
@@ -4,6 +4,7 @@ import androidx.core.text.HtmlCompat
 import com.dev.rawgapps.utils.RawgApiMock
 import com.google.common.truth.Truth
 import io.ktor.client.HttpClient
+import io.ktor.client.features.ClientRequestException
 import io.ktor.client.features.json.JsonFeature
 import io.ktor.client.features.json.serializer.KotlinxSerializer
 import io.mockk.coEvery
@@ -11,10 +12,12 @@ import io.mockk.mockkStatic
 import io.mockk.unmockkAll
 import kotlinx.coroutines.test.runTest
 import kotlinx.serialization.json.Json
+import org.junit.After
 import org.junit.Test
 
 class RawgRemoteDataSourceImplTest {
-    private val mockEngine = RawgApiMock().engine
+    private val apiMock = RawgApiMock()
+    private val mockEngine = apiMock.engine
 
     private val httpClient = HttpClient(engine = mockEngine) {
         install(JsonFeature) {
@@ -26,6 +29,12 @@ class RawgRemoteDataSourceImplTest {
         }
     }
     private val rawgRemoteDataSourceImpl = RawgRemoteDataSourceImpl(httpClient)
+
+    @After
+    fun cleanUp() {
+        apiMock.isFailed = false
+        unmockkAll()
+    }
 
     @Test
     fun given_params_then_success_call_api() {
@@ -39,20 +48,43 @@ class RawgRemoteDataSourceImplTest {
             })
         }
     }
-
+    @Test
+    fun given_params_then_failed() {
+        apiMock.isFailed = true
+        runTest {
+            rawgRemoteDataSourceImpl.getGames(1, 20).exceptionOrNull().apply {
+                Truth.assertThat(this).isNotNull()
+                Truth.assertThat(this).isInstanceOf(ClientRequestException::class.java)
+            }
+        }
+    }
 
     @Test
     fun when_getDetailGame_then_return_game() {
         mockkStatic(HtmlCompat::class)
         coEvery {
-            HtmlCompat.fromHtml(any(),any()).toString()
-        }returns "Testable"
+            HtmlCompat.fromHtml(any(), any()).toString()
+        } returns "Testable"
         runTest {
             rawgRemoteDataSourceImpl.getGameDetail("forza-motorsport-2020").getOrThrow().apply {
                 println(this)
                 Truth.assertThat(name).isEqualTo("Forza Motorsport")
             }
         }
-        unmockkAll()
+    }
+
+    @Test
+    fun when_getDetailGame_then_return_error() {
+        mockkStatic(HtmlCompat::class)
+        coEvery {
+            HtmlCompat.fromHtml(any(), any()).toString()
+        } returns "Testable"
+        apiMock.isFailed = true
+        runTest {
+            rawgRemoteDataSourceImpl.getGameDetail("forza-motorsport-2020").exceptionOrNull().apply {
+                Truth.assertThat(this).isNotNull()
+                Truth.assertThat(this).isInstanceOf(ClientRequestException::class.java)
+            }
+        }
     }
 }
